1f7979260d5f64f1ab9ffe31c7090fcf18987083
HarimKim <lklkcmk@naver.com>
2017-06-21 04:28:47 +0900

    aa

--- TreeSub.java
+++
@@ -1,6 +0,0 @@
-import java.util.ArrayList;
-
-public class TreeSub { //노드에 해당.
-	public ArrayList<TreeSub> list = new ArrayList<>(); // list = 이 노드의 child를 포함하는 list
-	public int data;
-}
--- ttest.java
+++
@@ -1,5 +0,0 @@
-public class ttest{
-	public static void main(String[] args){
-		
-		} 
-	}
